9549c226f7df6c276d83c4ab2c671d625b92ea73
albertoVictorRebello <albertorebello@gmail.com>
2022-03-05 18:35:53 -0300

    pass stage 2 check

--- minesweeper/Minesweeper/task/src/minesweeper/Main.kt
+++ minesweeper/Minesweeper/task/src/minesweeper/Main.kt
@@ -1,7 +1,10 @@
 package minesweeper
 
-fun main() {
-    // Stage 1
-    val minefield = Minefield()
+private fun main() {
+    // Stage 2
+    println("How many mines do you want on the field?")
+    val numberOfMines = readLine()!!.toInt()
+    val minefield = Minefield(numberOfMines = numberOfMines)
     print(minefield.printMinefield())
 }
+
--- minesweeper/Minesweeper/task/src/minesweeper/Minefield.kt
+++ minesweeper/Minesweeper/task/src/minesweeper/Minefield.kt
@@ -2,19 +2,50 @@ package minesweeper
 
 import kotlin.random.Random
 
+const val SAFE_SYMBOL = '.'
+const val MINE_SYMBOL = 'X'
+
 class Minefield(
     private val rows: Int = 9,
-    private val columns: Int = 9) {
+    private val columns: Int = 9,
+    private val numberOfMines: Int = 10) {
+
+    private var settedMines = 0
+    private var calculatedFactor = numberOfMines.toDouble() / (rows.toDouble() * columns.toDouble())
     private val minefield = MutableList<MutableList<Char>>(rows) {
-        MutableList<Char>(columns) { setCell() }}
+        MutableList<Char>(columns) { setCell(calculatedFactor) }}
+
+    init {
+        setCell(numberOfMines - settedMines)
+    }
+
+    // Defines if the sell is 'safe' or is a 'mine'
+    private fun setCell(factor: Double): Char {
+        val random = Random.Default
+        return if (random.nextDouble() <= factor && settedMines < numberOfMines) {
+            settedMines++
+            MINE_SYMBOL
+        } else SAFE_SYMBOL
+    }
+
+    private fun setCell(mineUnbalance: Int): Int {
+        val random = Random.Default
+        var counter = mineUnbalance
+        val replaceSymbol = if (counter > 0) MINE_SYMBOL else SAFE_SYMBOL
+        while (counter != 0) {
+            for (row in 0 until rows) {
+                for (column in 0 until columns) {
+                    if (minefield[row][column] != replaceSymbol) {
+                        minefield[row][column] = setCell(factor = calculatedFactor)
+                        counter -= if (minefield[row][column] == replaceSymbol) 1 else 0
+                    }
+                }
+            }
+        }
+        return counter
+    }
 
     fun printMinefield(): String {
         return minefield.map { it.joinToString("") }.joinToString("\n")
     }
-
-    fun setCell(): Char {
-        val random = Random.Default
-        return if (random.nextDouble() <=0.2) 'X' else '.'
-    }
-
 }
