9284ef6481244a50d244c73c07902b68671e63c6
albertoVictorRebello <albertorebello@gmail.com>
2022-03-05 18:40:31 -0300

    start stage 3

--- minesweeper/Minesweeper/task/src/minesweeper/Main.kt
+++ minesweeper/Minesweeper/task/src/minesweeper/Main.kt
@@ -1,7 +1,7 @@
 package minesweeper
 
 private fun main() {
-    // Stage 2
+    // Stage 3
     println("How many mines do you want on the field?")
     val numberOfMines = readLine()!!.toInt()
     val minefield = Minefield(numberOfMines = numberOfMines)
