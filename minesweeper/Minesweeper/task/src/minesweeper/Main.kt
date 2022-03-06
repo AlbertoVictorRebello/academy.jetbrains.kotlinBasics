4eb2b5dd2eda33e56883aa01718a814a017cb656
albertoVictorRebello <albertorebello@gmail.com>
2022-03-06 00:42:15 -0300

    pass stage 4 check

--- minesweeper/Minesweeper/task/src/minesweeper/Main.kt
+++ minesweeper/Minesweeper/task/src/minesweeper/Main.kt
@@ -1,10 +1,19 @@
 package minesweeper
 
 private fun main() {
-    // Stage 3
+    // Stage 4
     println("How many mines do you want on the field?")
     val numberOfMines = readLine()!!.toInt()
     val minefield = Minefield(numberOfMines = numberOfMines)
-    println(minefield.printMinefield(true))
+    println(minefield.printMinefield(true, true))
+
+    while (minefield.settedMines > 0) {
+        println("Set/delete mine marks (x and y coordinates):")
+        val (y, x) = readLine()!!.split(" ").map { it.toInt() }
+        println(minefield.checkCell(x, y))
+    }
+    print("Congratulations! You found all the mines!")
+
+
 }
 
--- minesweeper/Minesweeper/task/src/minesweeper/Minefield.kt
+++ minesweeper/Minesweeper/task/src/minesweeper/Minefield.kt
@@ -1,20 +1,23 @@
 package minesweeper
 
-import kotlin.math.min
 import kotlin.random.Random
 
-const val SAFE_SYMBOL = '.'
+const val MARKED_SYMBOL = '.'
 const val MINE_SYMBOL = 'X'
+const val UNMARKED_SYMBOL = '\u002A'
 
 class Minefield(
     private val rows: Int = 9,
     private val columns: Int = 9,
     private val numberOfMines: Int = 10) {
 
-    private var settedMines = 0
+    internal var settedMines = 0
+    get() = field
     private var calculatedFactor = numberOfMines.toDouble() / (rows.toDouble() * columns.toDouble())
     private val minefield = MutableList<MutableList<Char>>(rows) {
         MutableList<Char>(columns) { setCell(calculatedFactor) }}
+    private val minefieldCopy = MutableList<MutableList<Char>>(rows) {
+        MutableList<Char>(columns) { MARKED_SYMBOL }}
 
     init {
         setCell(numberOfMines - settedMines)
@@ -26,13 +29,13 @@ class Minefield(
         return if (random.nextDouble() <= factor && settedMines < numberOfMines) {
             settedMines++
             MINE_SYMBOL
-        } else SAFE_SYMBOL
+        } else MARKED_SYMBOL
     }
 
     private fun setCell(mineUnbalance: Int): Int {
         val random = Random.Default
         var counter = mineUnbalance
-        val replaceSymbol = if (counter > 0) MINE_SYMBOL else SAFE_SYMBOL
+        val replaceSymbol = if (counter > 0) MINE_SYMBOL else MARKED_SYMBOL
         while (counter != 0) {
             for (row in 0 until rows) {
                 for (column in 0 until columns) {
@@ -46,9 +49,8 @@ class Minefield(
         return counter
     }
 
-
     private fun checkMinesAround(elementRow: Int, elementColumn: Int): Char {
-        if (minefield[elementRow][elementColumn] == MINE_SYMBOL) return MINE_SYMBOL
+        if (minefield[elementRow][elementColumn] == MINE_SYMBOL) return MARKED_SYMBOL
 
         val baseRow = if (elementRow - 1 >= 0) elementRow - 1 else 0
         val baseColumn = if (elementColumn - 1 >= 0) elementColumn - 1 else 0
@@ -65,19 +67,56 @@ class Minefield(
             }
         }
 
-        return if (counter > 0) counter.toString().first() else SAFE_SYMBOL
+        return if (counter > 0) counter.toString().first() else MARKED_SYMBOL
     }
 
-    fun printMinefield(numberView: Boolean = false): String {
+    fun checkCell(row: Int, column: Int): String {
+        return if (minefieldCopy[row - 1][column - 1].isDigit()) {
+            "There is a number here!"
+        } else if (minefield[row - 1][column - 1] == MINE_SYMBOL) {
+            if (minefieldCopy[row - 1][column - 1] == MARKED_SYMBOL) {
+                minefieldCopy[row - 1][column - 1] = UNMARKED_SYMBOL
+                settedMines--
+            } else {
+                minefieldCopy[row - 1][column - 1] = MARKED_SYMBOL
+                settedMines++
+            }
+            printMinefield(true, false)
+        } else {
+            if (minefieldCopy[row - 1][column - 1] == UNMARKED_SYMBOL) {
+                minefieldCopy[row - 1][column - 1] = MARKED_SYMBOL
+            } else {
+                minefieldCopy[row - 1][column - 1] = UNMARKED_SYMBOL
+            }
+            printMinefield(true, false)
+        }
+    }
+    fun printMinefield(numberView: Boolean = false, resetNumberView: Boolean = false): String {
         if (numberView) {
-            val minefieldCopy = MutableList<MutableList<Char>>(rows) {
-                MutableList<Char>(columns) { SAFE_SYMBOL }}
-            for (row in 0 until minefield.size) {
-                for (column in 0 until minefield[row].size) {
-                    minefieldCopy[row][column] = checkMinesAround(row, column)
+            if (resetNumberView) {
+                for (row in 0 until minefield.size) {
+                    for (column in 0 until minefield[row].size) {
+                        minefieldCopy[row][column] = checkMinesAround(row, column)
+                    }
                 }
             }
-            return minefieldCopy.map {it.joinToString ( "" )}.joinToString("\n")
+
+            var rowCounter: Int = 0
+            var columnCounter: Int = 0
+            var grid: String = ""
+
+            // Grid variable holds the frame with coordinates into which the minefield will be inserted into
+            grid = " |"
+            repeat(columns) { grid +=  (++columnCounter).toString() }
+            grid += "|\n"
+            grid += "-|" + "-".repeat(columns) + "|\n"
+
+            // Here the minefield content is inserted into the frame
+            grid += minefieldCopy.map {(++rowCounter).toString() + "|" + it.joinToString ( "" )}.joinToString("|\n") + "|"
+
+            grid += "\n-|" + "-".repeat(columns) + "|"
+
+            return grid
 
         } else {
             return minefield.map { it.joinToString("") }.joinToString("\n")
